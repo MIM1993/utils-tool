bd04f8a5a082e495b42259420d2eea03e57dadd6
MIM <49119581+MIM1993@users.noreply.github.com>
2022-07-19 14:34:37 +0800

    Update time_util.go
    添加获取网络时间函数

--- time_utils/time_util.go
+++ time_utils/time_util.go
@@ -51,3 +51,34 @@ func Stamp2Time(stamp int64) time.Time {
 	timer := Str2Time(stampStr)
 	return timer
 }
+
+//获取网络上的时间
+func GetRemoteTime() (*time.Time, error) {
+	var host string = "time.windows.com:123"
+	// 182.92.12.11:123 是阿里的ntp服务器，可以换成其他域名的
+	conn, err := net.Dial("udp", host)
+	if err != nil {
+		//log.Fatalf("failed to connect: %v", err)
+		return nil, err
+	}
+	defer conn.Close()
+	if err := conn.SetDeadline(time.Now().Add(15 * time.Second)); err != nil {
+		//log.Fatalf("failed to set deadline: %v", err)
+		return nil, err
+	}
+	req := &packet{Settings: 0x1B}
+	if err := binary.Write(conn, binary.BigEndian, req); err != nil {
+		//log.Fatalf("failed to send request: %v", err)
+		return nil, err
+	}
+	rsp := &packet{}
+	if err := binary.Read(conn, binary.BigEndian, rsp); err != nil {
+		//log.Fatalf("failed to read server response: %v", err)
+		return nil, err
+	}
+	secs := float64(rsp.TxTimeSec) - ntpEpochOffset
+	nanos := (int64(rsp.TxTimeFrac) * 1e9) >> 32
+	showtime := time.Unix(int64(secs), nanos)
+	return &showtime, nil
+}
+
